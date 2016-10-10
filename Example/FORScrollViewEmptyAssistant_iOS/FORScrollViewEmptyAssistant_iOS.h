479c8b7cc75172d118717dca95aec4adaa677716
stubbornnessness <1219721814yxj>
2016-10-10 23:41:00 +0800

    1.0版本开始支持carthage

---
+++ Example/FORScrollViewEmptyAssistant_iOS/FORScrollViewEmptyAssistant_iOS.h
@@ -0,0 +1,19 @@
+//
+//  FORScrollViewEmptyAssistant_iOS.h
+//  FORScrollViewEmptyAssistant_iOS
+//
+//  Created by yuanxiaojun on 2016/10/10.
+//  Copyright © 2016年 XcodeYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FORScrollViewEmptyAssistant_iOS.
+FOUNDATION_EXPORT double FORScrollViewEmptyAssistant_iOSVersionNumber;
+
+//! Project version string for FORScrollViewEmptyAssistant_iOS.
+FOUNDATION_EXPORT const unsigned char FORScrollViewEmptyAssistant_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FORScrollViewEmptyAssistant_iOS/PublicHeader.h>
+
+
